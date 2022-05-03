69f2d8e9f90f5d1b8256bf93a221c2feb276a559
Vladimir Olennikov <ath31st@yandex.ru>
2022-05-03 17:13:19 +0300

    Start project "first telegram bot"

---
+++ src/main/java/org/example/botfarm/App.java
@@ -0,0 +1,13 @@
+package org.example.botfarm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
