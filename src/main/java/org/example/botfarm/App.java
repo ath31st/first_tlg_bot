68f0601a10edc9cff1c952d407019c63b0f9b950
Vladimir Olennikov <ath31st@yandex.ru>
2022-05-03 18:15:16 +0300

    project "first telegram bot", version 1.2 (fix vulnerability)

--- src/main/java/org/example/botfarm/App.java
+++ src/main/java/org/example/botfarm/App.java
@@ -2,13 +2,10 @@ package org.example.botfarm;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Bot firstBot = new Bot("no_first_bot", "5376392406:AAF0t69qvbtP2pUnr3HAA_KimVujBQ-CUxk");
+public class App {
+    public static void main(String[] args) {
+        Bot firstBot = new Bot(args[0], args[1]);
         firstBot.botConnect();
     }
 }
