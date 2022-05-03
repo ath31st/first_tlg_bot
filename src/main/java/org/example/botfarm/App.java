5c764ad88710e91c09572f24d1ea77bdc0ce32c8
Vladimir Olennikov <ath31st@yandex.ru>
2022-05-03 17:57:14 +0300

    project "first telegram bot", version 1.1

--- src/main/java/org/example/botfarm/App.java
+++ src/main/java/org/example/botfarm/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Bot firstBot = new Bot("no_first_bot", "5376392406:AAF0t69qvbtP2pUnr3HAA_KimVujBQ-CUxk");
+        firstBot.botConnect();
     }
 }
--- src/main/java/org/example/botfarm/Bot.java
+++ src/main/java/org/example/botfarm/Bot.java
@@ -1,4 +1,70 @@
 package org.example.botfarm;
 
-public class Bot {
+
+import org.apache.log4j.Logger;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Bot extends TelegramLongPollingBot {
+    private static final Logger log = Logger.getLogger(Bot.class);
+
+    final int RECONNECT_PAUSE = 10000;
+    String userName;
+    String token;
+
+    public Bot(String userName, String token) {
+        this.userName = userName;
+        this.token = token;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return userName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        log.debug("new Update receive");
+    }
+
+    public void botConnect() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(this);
+            log.info("TelegramAPI started. Look for messages");
+        } catch (TelegramApiException e) {
+            log.error("Cant Connect. Pause " + RECONNECT_PAUSE / 1000 + "sec and try again. Error: " + e.getMessage());
+            try {
+                Thread.sleep(RECONNECT_PAUSE);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+                return;
+            }
+            botConnect();
+        }
+    }
 }
