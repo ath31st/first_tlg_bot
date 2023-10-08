b826bf2f234fdf7f95870b8ecf0aff38ca662059
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-08 18:49:41 +0300

    added KDoc for JokeService

--- src/main/kotlin/org/example/botfarm/service/JokeService.kt
+++ src/main/kotlin/org/example/botfarm/service/JokeService.kt
@@ -4,15 +4,32 @@ import java.io.IOException
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
+/**
+ * This class provides functionality to retrieve jokes from the "http://www.bashorg.org" website.
+ *
+ * @property url The URL of the website from which jokes are retrieved.
+ * @property userAgent The user agent string used for HTTP requests.
+ */
 class JokeService {
     private val url = "http://www.bashorg.org/casual"
     private val userAgent = "Mozilla/5.0"
 
+    /**
+     * Retrieves a joke from the specified website.
+     *
+     * @return A joke as a string, or an error message if there are issues with the website.
+     */
     fun getJoke(): String {
         val rawDoc = getRawDataFromBashOrg()
         return parseFromRawData(rawDoc)
     }
 
+    /**
+     * Retrieves raw HTML data from the "http://www.bashorg.org" website.
+     *
+     * @return A Document object representing the raw HTML data, or null if there are issues
+     * with the connection.
+     */
     private fun getRawDataFromBashOrg(): Document? {
         var document: Document? = null
         try {
@@ -26,6 +43,13 @@ class JokeService {
         return document
     }
 
+    /**
+     * Parses jokes from raw HTML data.
+     *
+     * @param rawData The raw HTML data as a Document object.
+     * @return A joke as a string, or an error message if there are issues with parsing or no
+     * jokes are found.
+     */
     private fun parseFromRawData(rawData: Document?): String {
         var result: String? = null
         rawData!!.outputSettings(Document.OutputSettings().prettyPrint(false))
@@ -43,3 +67,4 @@ class JokeService {
         return result
     }
 }
+
