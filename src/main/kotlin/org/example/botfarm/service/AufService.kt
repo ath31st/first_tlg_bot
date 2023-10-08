9b06a54b0bb2705a2804da6b011c007aafa44e5e
Vladimir Olennikov <ath31st@yandex.ru>
2023-10-08 18:47:56 +0300

    added KDoc for AufService

--- src/main/kotlin/org/example/botfarm/service/AufService.kt
+++ src/main/kotlin/org/example/botfarm/service/AufService.kt
@@ -5,16 +5,36 @@ import java.io.IOException
 import java.security.SecureRandom
 import kotlin.random.Random
 
+/**
+ * This class provides functionality related to the "Auf" resources.
+ * It allows you to retrieve a random "Auf" text from a file and get a random "Auf" image file
+ * from resources.
+ *
+ * @property pathToTextAuf The path to the text file containing "Auf" phrases.
+ * @property pathToImagesAuf The path to the directory containing "Auf" image files.
+ */
 class AufService {
     private val pathToTextAuf = "./fb_resources/auf.txt"
     private val pathToImagesAuf = "./fb_resources/auf_images"
 
+    /**
+     * Retrieves a random "Auf" text from the specified file.
+     *
+     * @return A random "Auf" text.
+     */
     fun getAuf(): String {
         val aufList = readFileAsLinesUsingBufferedReader()
         val secureRandom = SecureRandom()
         return aufList[secureRandom.nextInt(aufList.size)]
     }
 
+    /**
+     * Reads the lines from the specified text file using a BufferedReader and returns them as a
+     * list of strings.
+     *
+     * @return A list of strings containing the lines from the file, or an error message if the
+     * file is not found.
+     */
     private fun readFileAsLinesUsingBufferedReader(): List<String> {
         val resultList = mutableListOf<String>()
         try {
@@ -29,6 +49,11 @@ class AufService {
         return resultList
     }
 
+    /**
+     * Retrieves a random "Auf" image file from the resources directory.
+     *
+     * @return A random "Auf" image file or null if no image files are found.
+     */
     fun getRandomAufFileFromResources(): File? {
         val resourceDirectory = File(pathToImagesAuf)
         if (resourceDirectory.exists() && resourceDirectory.isDirectory) {
